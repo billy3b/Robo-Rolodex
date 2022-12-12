c9a85d3076b8fef3496400f123d880ef6d28f24f
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2022-12-12 21:04:33 +0530

    Add files via upload

---
+++ src/Avatar.js
@@ -0,0 +1,27 @@
+const Avatar = [
+  {
+    id: '#1',
+    name:'subs',
+  },
+  {
+    id:'#2',
+    name:'amy',
+  },
+  {
+    id:'#3',
+    name:'yihwa',
+  },
+  {
+    id:'#4',
+    name:'rollx',
+  },
+  {
+    id:'#5',
+    name:'Dru',
+  },
+  {
+    id:'#6',
+    name:'KDJ',
+  }
+];
+export default Avatar;
---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBox = ({searchfield, searchChange}) => {
+  return(
+    <div >
+      <input className='dark-purple ba b--light-blue br3 pa3'
+      type ='search'
+      placeholder = 'Search your monster'
+      onChange = {searchChange}
+       />
+    </div>
+  );
+}
+export default SearchBox;
